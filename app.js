dd824218d2bb524d99d6c8a6125ef07d6d947e31
Bret Main <bretaudio@gmail.com>
2018-07-28 23:03:51 -0700

    Initial Commit

---
+++ app.js
@@ -0,0 +1,29 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io   = require('socket.io')(http);
+
+app.get('/', function(req, res){
+  res.sendFile('index.html', { root: __dirname});
+});
+
+app.get('/style.css', function (req, res) {
+  res.sendFile('style.css', { root: __dirname});
+})
+
+io.on('connection', function(socket){
+  socket.on('disconnect', function(){
+    console.log('User Disconnection');
+  });
+
+});
+
+io.on('connection', function(socket){
+  socket.on('chatMessage', function(msg){
+    io.emit('chatMessage', msg);
+    console.log('message: ' + msg);
+  });
+});
+
+http.listen(8000, function(){
+  console.log('listening on *:8000');
+});
